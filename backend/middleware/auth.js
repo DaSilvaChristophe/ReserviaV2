ee7415403057b3a55d287555fb6e7ea3d8c226b8
Cris <Crispsy88@msn.com>
2022-02-24 19:34:51 +0100

    Création systeme d'auth users

---
+++ backend/middleware/auth.js
@@ -0,0 +1,18 @@
+const jsonwebtoken = require('jsonwebtoken');
+
+module.exports = (req, res, next) => {
+    try {
+        const token = req.headers.authorization.split('')[1];
+        const decodedToken = jsonwebtoken.verify(token, 'RANDOM_TOKEN');
+        const userId = decodedToken.userId;
+        if (req.body.userId && req.body.userId !== userId) {
+            throw 'Utilisateur non trouvé !';
+        } else {
+            next();
+        }
+    } catch {
+        res.status(401).json({
+            error: new Error('Requête invalide !')
+        });
+    }
+};
